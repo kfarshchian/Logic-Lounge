8baa372242cff96c46f8542f6148d538f2920fb5
Kameron <kfarshchian@gmail.com>
2023-03-26 13:42:31 -0600

    chatroom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/home/home';
 import Login from './components/login or signup/login';
 import Profile from './components/profile/profile';
 import Tutor from './components/tutormatch/tutor';
+import Chatroom from './components/chatroom/chatroom';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="login" index element={<Login />} />
         <Route path="profile" index element={<Profile />} />
         <Route path="tutor" index element={<Tutor />} />
+        <Route path="chatroom" index element={<Chatroom />} />
       </Route>
     </Routes>
   );
---
+++ src/components/chatroom/chatroom.js
@@ -0,0 +1,10 @@
+const Chatroom = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Chatroom
