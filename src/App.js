fc1b4155c83862ade55ad82a88b56043ddccb402
Kameron <kfarshchian@gmail.com>
2023-03-26 13:55:14 -0600

    fix layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout/layout';
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return (
         <div>
-            
+            <p>hi</p>
         </div>
     )
 }
