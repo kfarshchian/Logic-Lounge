ec0cc029926427da1e313e1291cd2e8d77067f50
Gustavo Carrillo (He/Him) <115580058+0zrk23@users.noreply.github.com>
2023-04-03 17:31:38 -0600

    improved navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
-import { Typography } from '@mui/material';
+import {motion} from 'framer-motion'
 import "./navbar.scss"
 
-const pageNames = ['Find a Match','Chatroom']
-const pageLinks = ['/match','/chatroom']
+const pageNames = ['Home', 'Profile','Find a Match','Chatroom']
+const pageLinks = ['/','/profile','/match','chatroom']
 
 const Header = () => {
   const logout = (event) => {
@@ -13,67 +13,106 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <div className='navbar'>
-      <div className='logo'>
-        <Link to='/'>
-          {/**Logo goes here */}
-          <Typography 
-            sx={{
-              fontFamily: 'monospace',
-            }}
-            variant='h5'
-          >
-            Logic Lounge
-          </Typography>
-        </Link>
+    <>
+      <div className='navbar'>
+        <div className='logo'>
+          <Link to='/'>
+            {/**Logo goes here */}
+            <h5 className='logo-text'>
+              Logic Lounge
+            </h5>  
+          </Link>
+        </div>
+        <div className='page-links'>
+          {
+            pageNames.map((page,index) => (
+              <motion.div
+                className='motion-container'
+                initial={{
+                  
+                }}
+                whileHover={{
+                  scale: 1.1,
+                  transition: {
+                    duration: 0.1
+                  },
+                }}
+                whileTap={{
+                  scale: 0.9
+                }}
+                key={`${page}`}
+              >
+                <Link 
+                  className='page-link'
+                  to={`${pageLinks[index]}`}
+                >
+                  {page}
+                </Link>
+              </motion.div>
+            ))
+          }
+        </div>
+        <div className='user-links'>
+          {Auth.loggedIn() ? (
+            <>
+              <button onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <motion.div
+                className='motion-container'
+                initial={{
+                  
+                }}
+                whileHover={{
+                  scale: 1.1,
+                  transition: {
+                    duration: 0.1
+                  },
+                }}
+                whileTap={{
+                  scale: 0.9
+                }}
+              >
+                <Link 
+                  className='user-link'
+                  to="/login"
+                >
+                  Login
+                </Link>
+              </motion.div>
+              <motion.div
+                className='motion-container'
+                initial={{
+                  
+                }}
+                whileHover={{
+                  scale: 1.1,
+                  transition: {
+                    duration: 0.1
+                  },
+                }}
+                whileTap={{
+                  scale: 0.9
+                }}
+              >
+                <Link 
+                  className='user-link'
+                  to='/signup'
+                >
+                  Sign Up
+                </Link>
+              </motion.div>
+            </>
+          )}
+        </div>
+        <div className='navbar app'>
+          
+        </div>
       </div>
-      <div className='page-links'>
-        <Link className='page-link' to='/'>
-          Home
-        </Link>
-        {
-          pageNames.map((page,index) => (
-            <Link 
-              className='page-link'
-              key={`${page}`} 
-              to={`${pageLinks[index]}`}
-            >
-              {page}
-            </Link>
-          ))
-        }
-        <Link 
-          className='page-link'
-          to='/profile'
-        >
-          Profile
-        </Link>
-      </div>
-      <div className='user-links'>
-        {Auth.loggedIn() ? (
-          <>
-            <button onClick={logout}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link 
-              className='user-link'
-              to="/login"
-            >
-              Login
-            </Link>
-            <Link 
-              className='user-link'
-              to='/signup'
-            >
-              Sign Up
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
