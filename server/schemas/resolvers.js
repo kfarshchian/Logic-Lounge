78596541924e947425bac7ab808ad29982997b9c
ValentaCodes <valentacodes@gmail.com>
2023-04-05 01:35:52 -0600

    added functionality to allow skills to be added/removed to users and database

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,17 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought, Tutor } = require('../models');
+const { User, Thought, Tutor, Skill } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate('thoughts');
+      return User.find().populate('skills').lean();
     },
     user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
+      return User.findOne({ username });
+    },
+    skills: async () => {
+      return Skill.find();
     },
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
@@ -23,20 +26,54 @@ const resolvers = {
   },
 
   Mutation: {
-    addTutor: async (parent, {tutorName, skills}) => {
-      const tutor = await Tutor.create({tutorName, skills});
-      return {tutor}
+    addTutor: async (parent, { tutorName, skills }) => {
+      const tutor = await Tutor.create({ tutorName, skills });
+      return { tutor };
     },
     removeTutor: async (parent, { tutorId }) => {
       return Tutor.findOneAndDelete({ _id: tutorId });
     },
     updateTutor: async (parent, { tutorId, tutorName, bio, img, skills }) => {
-      return Tutor.findOneAndUpdate({ _id: tutorId, tutorName, bio, img, skills });
+      return Tutor.findOneAndUpdate({
+        _id: tutorId,
+        tutorName,
+        bio,
+        img,
+        skills,
+      });
     },
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
+    },
+    // This creates a new skill in database DOESNT ASSIGN TO USER
+    addNewSkill: async (parent, { skillName }) => {
+      return Skill.create({ skillName: skillName });
+    },
+    // Allows you to add a skill from database to a user
+    addSkillToUser: async (parent, { userId, skillId }) => {
+      const skill = await Skill.findById(skillId);
+      return User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $addToSet: { skills: { _id: skillId, skillName: skill.skillName } },
+        },
+        {
+          new: true,
+          // runValidators: true,
+        }
+      ).lean();
+    },
+    removeSkillFromUser: async (parent, { skillId, userId }) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { skills: { _id: skillId } } },
+        { new: true }
+      ).lean();
+    },
+    removeSkill: async (parent, { skillId }) => {
+      return Skill.findByIdAndDelete(skillId).lean();
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
