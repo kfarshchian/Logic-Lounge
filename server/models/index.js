518b312e7238d0e308a5564a06fea4b4c440c401
Gustavo Carrillo (He/Him) <115580058+0zrk23@users.noreply.github.com>
2023-04-05 16:46:37 -0600

    updated index.js to include Chatroom model

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@ const User = require('./User');
 const Thought = require('./Thought');
 const Tutor = require('./Tutor');
 const Skill = require('./Skill');
+const Chatroom = require('./Chatroom')
 
-module.exports = { User, Thought, Tutor, Skill };
+module.exports = { User, Thought, Tutor, Skill, Chatroom };
