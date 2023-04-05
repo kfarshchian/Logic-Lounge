a5d6e967f6d57d64c74ff88165a24ce366b5d2a2
ValentaCodes <valentacodes@gmail.com>
2023-04-05 11:50:03 -0600

    Removed login JS APRIL 5 2023

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,10 +4,16 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
-import { Box, Typography } from '@mui/material';
+import {
+  Button,
+  TextField,
+  FormControl,
+  Typography,
+  Box
+} from '@mui/material';
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ username: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -36,50 +42,76 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      email: '',
+      username: '',
       password: '',
     });
   };
 
   return (
-    <>
-      <Typography variant='h4'>Login</Typography>
-      <Box>
-        {data ? (
-          <p>
-            Success! You may now head <Link to='/'>back to the homepage.</Link>
-          </p>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            <input
-              className='form-input'
-              placeholder='Your email'
-              name='email'
-              type='email'
-              value={formState.email}
-              onChange={handleChange}
-            />
-            <input
-              className='form-input'
-              placeholder='******'
-              name='password'
-              type='password'
-              value={formState.password}
-              onChange={handleChange}
-            />
-            <button
-              className='btn btn-block btn-primary'
-              style={{ cursor: 'pointer' }}
-              type='submit'
+      <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant='h6'>Sign Up</Typography>
+        <div>
+          {data ? (
+            <p>
+              Success! You may now head{' '}
+              <Link to='/'>back to the homepage.</Link>
+            </p>
+          ) : (
+            <FormControl
+              onSubmit={handleFormSubmit}
+              sx={{
+                border: 1,
+                borderColor: '#4F2683',
+                width: '20rem',
+                padding: 5,
+                borderRadius: 11,
+                boxShadow: '3',
+              }}
             >
-              Submit
-            </button>
-          </form>
-        )}
-
-        {error && (
-          <div className='my-3 p-3 bg-danger text-white'>{error.message}</div>
-        )}
+              <form>
+                <TextField
+                  margin='normal'
+                  variant='outlined'
+                  required
+                  label='Username'
+                  name='username'
+                  type='username'
+                  onChange={handleChange}
+                  value={formState.username}
+                />
+                <TextField
+                  margin='normal'
+                  variant='outlined'
+                  required
+                  label='Password'
+                  name='password'
+                  type='password'
+                  onChange={handleChange}
+                  value={formState.password}
+                />
+                <Button
+                  color='secondary'
+                  sx={{ cursor: 'pointer', color: '#4F2683' }}
+                  type='submit'
+                  variant='outlined'
+                >
+                  Submit
+                </Button>
+              </form>
+            </FormControl>
+          )}
+          {error && (
+            <div className='my-3 p-3 bg-danger text-white'>{error.message}</div>
+          )}
+        </div>
       </Box>
     </>
   );
