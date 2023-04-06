5b7dac32c353bb40913a8c08c22a68d700b71ea3
Gustavo Carrillo (He/Him) <115580058+0zrk23@users.noreply.github.com>
2023-04-05 18:58:07 -0600

    hotfix for proxy error

--- client/src/pages/Chatrooms/Chatrooms.jsx
+++ client/src/pages/Chatrooms/Chatrooms.jsx
@@ -2,7 +2,7 @@ import {io} from 'socket.io-client'
 
 
 function Chatrooms() {
-  const socket = io();
+  const socket = io('http://localhost:3001');
   socket.emit('test-query','This is a query')
   return (
     <div>Chatrooms</div>
