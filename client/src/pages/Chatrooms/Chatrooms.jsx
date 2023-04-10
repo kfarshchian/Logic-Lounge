c65140d6d98ad06eda5b41a3db810ff9fef2225d
guscarrillo <115580058+guscarrillo@users.noreply.github.com>
2023-04-09 18:06:07 -0600

    added basic display to chatrooms

--- client/src/pages/Chatrooms/Chatrooms.jsx
+++ client/src/pages/Chatrooms/Chatrooms.jsx
@@ -1,19 +1,53 @@
 import { useEffect, useState } from 'react'
-import {io} from 'socket.io-client'
+import ChatroomNav from '../../components/ChatroomNav';
 
-function Chatrooms() {
-  const [chatrooms, setChatrooms] = useState([])
-  const socket = io();
 
+
+function Chatrooms({socket}) {
+  const [chatrooms,setChatrooms] = useState([]);  
+  const [selectedChat,setSelectedChat] = useState(null);
+
+  //use effect fires on page open. Queries chatrooms
   useEffect(()=>{
-    socket.emit('QUERY_CHATROOMS')  
-    socket.on('QUERY_CHATROOMS',(data)=>{
-      setChatrooms(data);
-    })
-  },[socket])
+    socket.emit('QUERY_CHATROOMS')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
+  //Socket listener for updated chatrooms; will update all of the current chatrooms
+  socket.on('UPDATED_CHATROOMS', async (data)=>{
+    setChatrooms(await JSON.parse(data));
+  })
+
+  //Handler for selecting chatroom
+  const selectChatroom  = (e) => {
+    const id = e.target.getAttribute('chat-id');
+    setSelectedChat(chatrooms.find(chatroom => chatroom._id === id))
+  }
 
   return (
-    <div>{JSON.stringify(chatrooms)}</div>
+    <div>
+      {/* Side navebar for the chatrooms */}
+      <ChatroomNav chatrooms={chatrooms} selectChatroom={selectChatroom} selectedChatroom={selectedChat}/>
+      {/* If a chat is selected, display the chatname, otherwise display select chatroom */}
+      {selectedChat ? (
+        <div>
+          {selectedChat.chatroomName}
+        </div>
+      ) : (
+        <div>
+          Select a Chatroom
+        </div>
+      )}
+      {/* If a chat is selected display messages, otherise display nothing */}
+      {selectedChat && (
+        selectedChat.messages.map((message,index) => (
+          <div key={index}>
+            {JSON.stringify(message)}
+          </div>
+        ))
+      )}
+      {/* This will be a form eventually */}
+    </div>
   )
 }
 
