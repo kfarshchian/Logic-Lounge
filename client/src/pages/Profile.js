913d2079b53f018ea1c42b84e118ba7a2a23f06e
ValentaCodes <valentacodes@gmail.com>
2023-04-16 22:05:35 -0600

    updated UI and responsiveness APRIL 16 2023

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import {
@@ -20,13 +18,10 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useMutation } from '@apollo/client';
 import { useState } from 'react';
-import { QUERY_SINGLE_USER } from "../utils/queries";
+import { QUERY_SINGLE_USER } from '../utils/queries';
 import { UPDATE_USER } from '../utils/mutations';
 import { QUERY_SKILLS } from '../utils/queries';
-import { MATCH_TUTOR } from "../utils/queries";
-
-
-
+import { MATCH_TUTOR } from '../utils/queries';
 
 const ProfileBox = styled(Box)(({ theme }) => ({
   display: 'flex',
@@ -59,22 +54,22 @@ const MatchImage = styled(Avatar)(({ theme }) => ({
 function Profile() {
   // This will query the available skills from the database
   const { data: skillData } = useQuery(QUERY_SKILLS);
-  const { loading, error,  data: tutorData } = useQuery(MATCH_TUTOR);
+  const { loading, error, data: tutorData } = useQuery(MATCH_TUTOR);
   console.log(tutorData);
   const { userId } = useParams();
 
   const { data: userName } = useQuery(QUERY_SINGLE_USER, {
     variables: { id: userId },
   });
-console.log(userName);
+  console.log(userName);
   // This will access the gql mutation by accessing the needed userId variable
   const [updateUser] = useMutation(UPDATE_USER, {
-    variables: {userId}
+    variables: { userId },
   });
-  
-  const [profilePicture, setProfilePicture] = useState({
-    img: '',
-  });
+
+  // const [profilePicture, setProfilePicture] = useState({
+  //   img: '',
+  // });
 
   const [editing, setEditing] = useState(false);
 
@@ -132,7 +127,7 @@ console.log(userName);
 
   const styles = {
     root: {
-      backgroundColor: '#DDCDC6',
+      // backgroundColor: '#DDCDC6',
       flexGrow: 1,
       paddingTop: '18px',
       paddingBottom: '18px',
@@ -162,130 +157,126 @@ console.log(userName);
 
   return (
     <div style={styles.root}>
-      <Grid container spacing={10} justifyContent='flex=end'>
-        <Grid item xs={10} sm={4} md={3}>
-          <ProfileBox>
-            {user ? (
-              <>
-                <Avatar style={styles.avatar} />
-                <Typography variant='h6' style={styles.name}>
-                  {user.username}
-                </Typography>
-                <Typography variant='body1' style={styles.bio}>
-                  {/* {userData.skills} */}
-                </Typography>
-                {/* <Typography variant="body1" style={styles.interest}>
+      <Stack
+        direction={{ sm: 'column', md: 'row' }}
+        spacing={{ xs: 1, sm: 2, md: 3 }}
+        alignItems={'center'}
+      >
+        <ProfileBox sx={{ maxWidth: '20rem' }} alignItems={'center'}>
+          {user ? (
+            <>
+              <Avatar style={styles.avatar} sx={{ position: 'initial' }} />
+              <Typography variant='h6' style={styles.name}>
+                {user.username}
+              </Typography>
+              <Typography variant='body1' style={styles.bio}>
+                {/* {userData.skills} */}
+              </Typography>
+              {/* <Typography variant="body1" style={styles.interest}>
             Interest
           </Typography> */}
-              </>
-            ) : (
-              'hit'
-            )}
-            {!editing ? (
-              <Button
-                variant='contained'
-                color='primary'
-                style={styles.button}
-                onClick={handleEdit}
-              >
-                Edit Profile
-              </Button>
-            ) : (
-              <>
-                <TextField
-                  type='file'
-                  onChange={(event) => setProfilePicture(event.target.value)}
-                  label=''
-                  value={profilePicture.img}
-                />
-                  <FormControl margin='normal'>
-                    <form onSubmit={handleSave}>
-                    <InputLabel>Skills</InputLabel>
-                    <Select
-                      sx={{ minWidth: '20rem', maxWidth: '20rem' }}
-                      multiple
-                      id='selection'
-                      name='skills'
-                      value={formState.skills}
-                      onChange={handleChange}
-                      input={<OutlinedInput label='Skill' />}
-                      renderValue={(selected) => selected.join(',')}
-                      MenuProps={MenuProps}
-                    >
-                      {/* This will render skills dynamically from database */}
-                      {skillData
-                        ? skillData.skills.map((skill) => (
-                            <MenuItem key={skill._id} value={skill.skillName}>
-                              <Checkbox
-                                checked={
-                                  formState.skills.indexOf(skill.skillName) > -1
-                                }
-                              />
-                              <ListItemText primary={skill.skillName} />
-                            </MenuItem>
-                          ))
-                        : 'No skills available at this time...'}
-                    </Select>
+            </>
+          ) : (
+            'hit'
+          )}
+          {!editing ? (
+            <Button
+              variant='contained'
+              color='primary'
+              style={styles.button}
+              onClick={handleEdit}
+            >
+              Edit Profile
+            </Button>
+          ) : (
+            <>
+              {/* <TextField
+                type='file'
+                onChange={(event) => setProfilePicture(event.target.value)}
+                label=''
+                value={profilePicture.img}
+              /> */}
+              <FormControl margin='normal'>
+                <form onSubmit={handleSave}>
+                  <InputLabel>Skills</InputLabel>
+                  <Select
+                    sx={{ minWidth: '12rem', maxWidth: '12rem' }}
+                    multiple
+                    id='selection'
+                    name='skills'
+                    value={formState.skills}
+                    onChange={handleChange}
+                    input={<OutlinedInput label='Skill' />}
+                    renderValue={(selected) => selected.join(',')}
+                    MenuProps={MenuProps}
+                  >
+                    {/* This will render skills dynamically from database */}
+                    {skillData
+                      ? skillData.skills.map((skill) => (
+                          <MenuItem key={skill._id} value={skill.skillName}>
+                            <Checkbox
+                              checked={
+                                formState.skills.indexOf(skill.skillName) > -1
+                              }
+                            />
+                            <ListItemText primary={skill.skillName} />
+                          </MenuItem>
+                        ))
+                      : 'No skills available at this time...'}
+                  </Select>
                   <Button type='submit'>Save Profile</Button>
-                  </form>
-                  </FormControl>
-              </>
-            )}
-          </ProfileBox>
-        </Grid>
-         <Grid  item xs={12} sm={6} md={4} lg={5 }>
-            <Typography variant="h4" gutterBottom>
-              Matches your skills
-            </Typography>
-            <Stack direction="column" justifyContent="center">
-              <MatchBox>
-                <MatchImage src={tutorData.tutors[0].image}>
-                  </MatchImage>
-                <Typography variant="h6" gutterBottom>
-                  {tutorData.tutors[0].tutorName}
-                </Typography>
-                <Typography variant="body1">
-                  {tutorData.tutors[0].bio}
-                  </Typography>
-              </MatchBox>
-            </Stack>
-          </Grid>
-          <Grid  item xs={12} sm={6} md={4} lg={5 }>
-            <Typography variant="h4" gutterBottom>
-              Matches your skills
-            </Typography>
-            <Stack direction="column" justifyContent="center">
-              <MatchBox>
-                <MatchImage src={tutorData.tutors[1].image}>
-                  </MatchImage>
-                <Typography variant="h6" gutterBottom>
-                  {tutorData.tutors[1].tutorName}
-                </Typography>
-                <Typography variant="body1">
-                  {tutorData.tutors[1].bio}
-                  </Typography>
-              </MatchBox>
-            </Stack>
-          </Grid>
-          <Grid  item xs={12} sm={6} md={4} lg={5 }>
-            <Typography variant="h4" gutterBottom>
-              Matches your skills
-            </Typography>
-            <Stack direction="column" justifyContent="center">
-              <MatchBox>
-                <MatchImage src={tutorData.tutors[2].image}>
-                  </MatchImage>
-                <Typography variant="h6" gutterBottom>
-                  {tutorData.tutors[2].tutorName}
-                </Typography>
-                <Typography variant="body1">
-                  {tutorData.tutors[2].bio}
-                  </Typography>
-              </MatchBox>
-            </Stack>
-          </Grid>
-      </Grid>
-      
+                </form>
+              </FormControl>
+            </>
+          )}
+        </ProfileBox>
+        <Stack
+          direction={{ sm: 'column', md: 'row' }}
+          flexWrap={'wrap'}
+          spacing={{ xs: 1, sm: 2, md: 0 }}
+          alignItems={'center'}
+          justifyContent={'space-even'}
+        >
+          <Stack direction='column' justifyContent='center'>
+            <MatchBox minWidth={'15rem'}>
+              <Typography variant='h6' gutterBottom color={'white'}>
+                Matches your skills
+              </Typography>
+              <MatchImage src={tutorData.tutors[0].image}></MatchImage>
+              <Typography variant='h6' gutterBottom color={'white'}>
+                {tutorData.tutors[0].tutorName}
+              </Typography>
+              <Typography variant='h6'>{tutorData.tutors[0].bio}</Typography>
+            </MatchBox>
+          </Stack>
+          <Stack direction='column' justifyContent='center'>
+            <MatchBox minWidth={'15rem'}>
+              <Typography variant='h6' gutterBottom color={'white'}>
+                Matches your skills
+              </Typography>
+              <MatchImage src={tutorData.tutors[1].image}></MatchImage>
+              <Typography variant='h6' gutterBottom color={'white'}>
+                {tutorData.tutors[1].tutorName}
+              </Typography>
+              <Typography variant='h6'>{tutorData.tutors[1].bio}</Typography>
+            </MatchBox>
+          </Stack>
+          <Stack direction='column' justifyContent='center'>
+            <MatchBox minWidth={'15rem'}>
+              <Typography variant='h6' gutterBottom color={'white'}>
+                Matches your skills
+              </Typography>
+              <MatchImage src={tutorData.tutors[2].image}></MatchImage>
+              <Typography variant='h6' gutterBottom color={'white'}>
+                {tutorData.tutors[2].tutorName}
+              </Typography>
+              <Typography variant='h6' color={'white'} gutterBottom>
+                {tutorData.tutors[2].bio}
+              </Typography>
+            </MatchBox>
+          </Stack>
+        </Stack>
+      </Stack>
     </div>
   );
 }
